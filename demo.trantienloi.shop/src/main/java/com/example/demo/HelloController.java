5b676b575d199f0579bfe9e210357b865aece2fc
trantienloi <trantienloi412@gmail.com>
2024-10-22 19:13:24 +0700

    test

--- demo.trantienloi.shop/src/main/java/com/example/demo/HelloController.java
+++ demo.trantienloi.shop/src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@GetMapping("/")
 	public String Index() {
-		return "Hello spring boot";
+		return "hello spring boot";
 	}
 
 }
